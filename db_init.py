b2d833288feb76869435e399258b933e6f2a6c2b
Posiuu <piotr.krol505@gmail.com>
2022-01-08 12:33:30 +0100

    Cascade deleting + test_data creation condition

--- db_init.py
+++ db_init.py
@@ -1,6 +1,16 @@
 from open_science import db
+from open_science.models import create_essential_data
+
+
+# drops all tables from database not relying on models
+def drop_all_from_database():
+    connection = db.engine.connect()
+    connection.execute('DROP SCHEMA public CASCADE')
+    connection.execute('CREATE SCHEMA public')
+
+
+drop_all_from_database()
 db.create_all()
 db.session.commit()
 
-from open_science.models import create_essential_data 
 create_essential_data()
