b217e59ff3ee496b51561c82773e754c8a0e4e39
marek <marek.zieba@icloud.com>
2022-01-18 17:02:36 +0100

    some improvemenst to comments

--- open_science/forms.py
+++ open_science/forms.py
@@ -11,6 +11,7 @@ class CommentForm(FlaskForm):
     content = TextAreaField("Add comment", validators=[DataRequired(), Length(max=mc.COMMENT_TEXT_L)])
     refObjectType = HiddenField()
     refObjectID = HiddenField()
+    commentRef = HiddenField()
     submit_comment = SubmitField("Publish comment")
 
 class FileUploadForm(FlaskForm):
