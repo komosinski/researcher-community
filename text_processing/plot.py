a893f89b47161b51a8f65325f5b63b6d12948ace
Tomasz_Wojtyniak <tomasz.w@moodup.team>
2022-01-19 22:21:21 +0100

    Delete libraries and add matplotlib plot

--- text_processing/plot.py
+++ text_processing/plot.py
@@ -1,7 +1,5 @@
 from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
-import pandas as pd
-import seaborn as sns
 from search_engine import get_similar_users_to_user
 from similarity_matrix import get_similarities_matrix_from_db
 
@@ -14,8 +12,7 @@ def create_users_plot(users_dict_id):
         user_matrix.append(array)
     pca = PCA(n_components=2, whiten=False, random_state=42)
     standardized_pca = pca.fit_transform(user_matrix)
-    users_standardized_pca = pd.DataFrame(data=standardized_pca, columns=["x", "y"])
-    sns.scatterplot(x="x", y="y", data=users_standardized_pca)
+    plt.scatter(standardized_pca[:, 0], standarlized_pca[:, 1])
     ax.axes.xaxis.set_visible(False)
     ax.axes.yaxis.set_visible(False)
     plt.show()
