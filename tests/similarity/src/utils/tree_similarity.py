a345e7f14bffeb8535cacc9ad0b97c31b8f931bb
Stanisław Kowalczyk <44475611+stachuk99@users.noreply.github.com>
2023-05-15 22:48:04 +0200

    Changed similarity metrics to scikit

--- tests/similarity/src/utils/tree_similarity.py
+++ tests/similarity/src/utils/tree_similarity.py
@@ -1,39 +1,21 @@
-from clusim.clustering import Clustering, print_clustering
-import clusim.sim as sim
+from sklearn import metrics
 import json
 import os
-import networkx as nx
 
 
-def _add_leafs(dendrogram, cluster = 0, elm2clu_dict={}):
-    result = nx.DiGraph()
-    for direction in ['left', 'right']:
-        if type(dendrogram[direction]) is str:
-            elm2clu_dict[dendrogram[direction]] = [cluster]
-            result.add_node(cluster)
-            cluster += 1
-    for direction in ['left', 'right']:
-        if dendrogram[direction] and type(dendrogram[direction]) is not str:
-            result, elm2clu_dict, cluster = _add_leafs(dendrogram[direction],  cluster, elm2clu_dict)
-    return result, elm2clu_dict, cluster
-
-def _dict_to_cluster(dendrogram, result=None, cluster=0, clusters=[], elm2clu_dict={}):
+def _dict_to_cluster(dendrogram, result=None, cluster=0):
     if result is None:
-        result, elm2clu_dict, cluster = _add_leafs(dendrogram, cluster=0, elm2clu_dict={})
-    c = list()
-    for direction in ['left', 'right']:
-        if dendrogram[direction] and type(dendrogram[direction]) is not str:
-            result, cluster, elm2clu_dict = _dict_to_cluster(dendrogram[direction], result, cluster, clusters, elm2clu_dict)
-            c.append(int(cluster))
+        result = {}
     for direction in ['left', 'right']:
         if type(dendrogram[direction]) is str:
-            result.add_edge(cluster, elm2clu_dict[dendrogram[direction]][0])
-    for _c in c:
-        result.add_edge(cluster, _c - 1)
-    cluster += 1
-    return result, cluster, elm2clu_dict
+            result[dendrogram[direction]] = cluster
+    for direction in ['left', 'right']:
+        if dendrogram[direction] and type(dendrogram[direction]) is not str:
+            r, cluster = _dict_to_cluster(dendrogram[direction], result, cluster + 1)
+            result.update(r)
+    return result, cluster
 
-def tree_similarity(tree1, tree2, similarity_measure='jaccard_index'):
+def tree_similarity(tree1, tree2, similarity_measure='fowlkes_mallows_score'):
     """
     Calculates similarity measure of two trees given in our format
 
@@ -41,17 +23,16 @@ def tree_similarity(tree1, tree2, similarity_measure='jaccard_index'):
     ----------
     tree1 : First tree in our format.
     tree2 : Second tree in our format.
-    similarity_measure: Measure used to calculate similarity, available measures:
-    'jaccard_index', 'rand_index', 'adjrand_index', 'fowlkes_mallows_index', 'fmeasure', 'purity_index', 'classification_error',
-    'czekanowski_index', 'dice_index', 'sorensen_index', 'rogers_tanimoto_index', 'southwood_index', 'pearson_correlation', 'corrected_chance',
-    'sample_expected_sim', 'nmi', 'mi', 'adj_mi', 'rmi', 'vi', 'geometric_accuracy', 'overlap_quality', 'onmi', 'omega_index'
-
+    similarity_measure: Measure used to calculate similarity, available measures from scikit:
+    'rand_score', 'adjusted_rand_score', 'fowlkes_mallows_score'...
     Returns
     -------
     Measured similarity
     """
-    d1_cluster, _, elm2clu_dict1 = _dict_to_cluster(tree1)
-    clu1 = Clustering(hier_graph=d1_cluster, elm2clu_dict=elm2clu_dict1)
-    d2_cluster, _, elm2clu_dict2 = _dict_to_cluster(tree2)
-    clu2 = Clustering(hier_graph=d2_cluster, elm2clu_dict=elm2clu_dict2)
-    return eval('sim.' + similarity_measure + '(clu1, clu2)')
+    d1_cluster = _dict_to_cluster(tree1)[0]
+    d2_cluster = _dict_to_cluster(tree2)[0]
+    l1, l2 = [], []
+    for k in d1_cluster.keys():
+        l1.append(d1_cluster[k])
+        l2.append(d2_cluster[k])
+    return eval('metrics.' + similarity_measure + '(l1, l2)')
