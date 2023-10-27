47d4db28e3c49e11b9386f737ca9ea2e721a1de2
Bartek <szymk.b9@gmail.com>
2023-10-27 19:04:48 +0200

    feat: add users' initials to 2D plot

--- text_processing/plot.py
+++ text_processing/plot.py
@@ -23,17 +23,17 @@ def get_user_id_ranking_dict():
 
 def create_save_users_plot():
     fig, ax = plt.subplots(1, 1, figsize=(10, 10), dpi=100)
-
     user_id_ranking_dict = get_user_id_ranking_dict()
     ranking_list = [ranking for ranking in user_id_ranking_dict.values()]
+    users_ids_with_initals = get_users_ids_with_initials()
     if user_id_ranking_dict:
         pca = PCA(n_components=2, whiten=False, random_state=42)
         standardized_pca = pca.fit_transform(ranking_list)
-        plt.scatter(standardized_pca[:, 0], standardized_pca[:, 1])
+        plt.scatter(standardized_pca[:, 0], standardized_pca[:, 1], marker='')
         ax.axes.xaxis.set_visible(False)
         ax.axes.yaxis.set_visible(False)
         for i, id in enumerate(user_id_ranking_dict.keys()):
-            ax.annotate(id, (standardized_pca[:, 0][i], standardized_pca[:, 1][i]))
+            ax.annotate(users_ids_with_initals[id], (standardized_pca[:, 0][i], standardized_pca[:, 1][i]), color='blue')
 
     users_plot_url = os.path.join(app.config['ROOTDIR'], app.config['USERS_PLOT_2D_FILE_PATH'])
     plt.savefig(users_plot_url, dpi=200, bbox_inches="tight")
@@ -55,3 +55,9 @@ def create_save_users_plot_3d():
 
     users_plot_url_3d = os.path.join(app.config['ROOTDIR'], app.config['USERS_PLOT_3D_FILE_PATH'])
     fig.write_html(users_plot_url_3d)
+
+
+# Returns dict that contains ids of all users paired with names initials in uppercase
+def get_users_ids_with_initials():
+    users = User.query.filter(User.id != 0).all()
+    return { user.id: f'{user.first_name[0]}{user.second_name[0]}'.upper() for user in users}
