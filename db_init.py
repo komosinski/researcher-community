515f5263218770f8b386585311e3a515cb5bbf2a
komosinski <86693077+komosinski@users.noreply.github.com>
2024-08-18 02:24:55 +0200

    Introduced a choice of how to initialize (clear) the database

--- db_init.py
+++ db_init.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
 
-# drops all tables from database not relying on models
+# drops all tables from the database not relying on the models defined in the source - deletes everything
 def drop_all_from_database():
     connection = db.engine.connect()
     connection.execute('DROP SCHEMA public CASCADE')
@@ -16,8 +16,11 @@ def db_init():
     if not database_exists(init_engine.url):
         create_database(init_engine.url)
 
-    drop_all_from_database()
-    #db.drop_all() #This method drop tables relying on models
+    CLEAR_ALL = True  # delete everything from the database? useful when the model defined in the source has changed compared to the current database content
+    if CLEAR_ALL:
+        drop_all_from_database()
+    else:
+        db.drop_all()  # drops tables from the database according to the models defined in the source
     db.create_all()
     db.session.commit()
 
